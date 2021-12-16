2b269eb632a1950a18766d2db71bbcd08e0eb4b7
erpuneet151198 <puneetpahul90@gmail.com>
2021-12-16 16:44:14 +0530

    uploading crud function based

---
+++ enroll/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=40)
+    password= models.CharField(max_length=100)
+    
